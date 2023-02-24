72da9786f86005ae3499a842bb2180b56ac09b74
sisipho21 <tkssis001@myuct.ac.za>
2023-02-24 23:17:19 +0200

    Added non-functional header and implementation files

---
+++ FileReader.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "FileReader.h"
+
+int addToStruct()
+{
+    return 1;
+};
+int readInFile()
+{
+    return 2;
+};
+int printTagData()
+{
+    return 3;
+};
+int printAllData()
+{
+    return 4;
+};
---
+++ FileReader.h
@@ -0,0 +1,9 @@
+#ifndef _FILE_R
+#define _FILE_R
+
+int addToStruct();
+int readInFile();
+int printTagData();
+int printAllData();
+
+#endif
