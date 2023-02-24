f317b845e20151a4c052d4b4a4ae2752e2bb2b5b
sisipho21 <tkssis001@myuct.ac.za>
2023-02-24 13:14:28 +0200

    Added non-functional driver file

---
+++ driver.cpp
@@ -0,0 +1,10 @@
+#include <vector>   //including files that will be needed from libraries
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "The code is running" << std::endl;
+}
