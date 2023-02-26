5c3273c588123387630369ddd78731672cf123b7
sisipho21 <tkssis001@myuct.ac.za>
2023-02-26 12:27:35 +0200

    readInFile method can add tags to vector

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
-#include <ifstream>
+#include <fstream>
 #include <string>
-#include <sstring>
+#include <sstream>
+#include <vector>
 #include "FileReader.h"
 
+std::vector<TKSSIS001::TagStruct> tagVector;
 
-std::string TKSSIS001::readInFile(sdt::string fname)
+
+std::string TKSSIS001::readInFile(std::string fname)
 {
     //opening the file using ifstream object
-    ifstream myfile(fname);
+    std::ifstream myfile(fname);
     if (!myfile)
     {
         return "File " + fname +" could not be opened.";
@@ -27,6 +30,9 @@ std::string TKSSIS001::readInFile(sdt::string fname)
         std::string sTag = nextline.substr( (open_angle1 + 1), (close_angle1 - open_angle1 - 1) );
         std::string sText = nextline.substr( (close_angle1 + 1), (open_angle2 - close_angle1 - 1) );
 
+        //parse sTag and sText to addToStruct so that a TagStruct object is created and added to the vector
+        TKSSIS001::addToStruct(sTag, sText);
+
     }
 
     //closing the ifstream as we are done working with it
@@ -35,10 +41,34 @@ std::string TKSSIS001::readInFile(sdt::string fname)
 };
 
 
-std::string TKSSIS001::addToStruct()
+void TKSSIS001::addToStruct(std::string sTag, std::string sText)
 {
-    
-    return "addToStruct envoked";
+    int vsize = tagVector.size();
+    bool found = false; //flag to check if tag already exists
+
+    /*check through tagVector if the given tag already exists
+    *if it does, increment numTags and concatenate the text string for that tag
+    */
+    for (int i = 0; i < vsize; i++)
+    {
+        if (sTag == tagVector[i].tag)
+        {
+            tagVector[i].numTags += 1;
+            tagVector[i].text += sText;
+            found = true;
+        }
+        
+    }
+    //if tag does not exist, create a new TagStruct object and add it to the vector
+    if (!found)
+    {
+        TKSSIS001::TagStruct tagInfo;
+        tagInfo.tag = sTag;
+        tagInfo.numTags = 1;
+        tagInfo.text = sText;
+
+        tagVector.push_back(tagInfo);
+    }
 };
 
 std::string TKSSIS001::printTagData()
